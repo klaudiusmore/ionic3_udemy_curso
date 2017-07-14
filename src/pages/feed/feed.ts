3c651b350a31cf2bb4245e2434a8cb9d8e49abd5
unknown <klaudius.more@gmail.com>
2017-07-13 21:50:32 -0300

    first commit

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -14,13 +14,22 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class FeedPage {
 
+  public objeto_feed = {
+    titulo: "Claudio Santos",
+    data: "November 5, 1955",
+    descdricao: "Nunca foi tão intuitivo criar aplicativos em ionic 3, uma estrutura incrivel.",
+    qntd_likes: 0,
+    qntd_coments: 0,
+    time_coments: "11h ago"
+  }
+
   public nome_usuario: string = "Marco Feliciano";
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  public somaDoisNumeros(): void{
-    alert(2 + 10 );
+  public somaDoisNumeros(): void {
+    alert(2 + 10);
   }
 
   ionViewDidLoad() {
